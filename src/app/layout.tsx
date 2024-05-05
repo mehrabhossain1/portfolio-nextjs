6f1af02e0d9ff23f7d6f2c3acfdcd5e9715a33b3
mehrabhossain1 <mehrab.munna00@gmail.com>
2024-05-05 23:55:30 +0600

    Active link color

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import { Providers } from "./providers/providers";
+import Header from "@/components/Header/Header";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -18,7 +19,10 @@ export default function RootLayout({
   return (
     <html lang="en" className="light">
       <body className={inter.className}>
-        <Providers>{children}</Providers>
+        <Providers>
+          <Header />
+          <div className="max-w-[1250px] mx-auto">{children}</div>
+        </Providers>
       </body>
     </html>
   );
--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,27 +1,41 @@
-import React from "react";
-import { Navbar, NavbarContent, NavbarItem, Link } from "@nextui-org/react";
+"use client";
+
+import { Navbar, NavbarContent, NavbarItem } from "@nextui-org/react";
+import Link from "next/link";
+import { usePathname } from "next/navigation";
 
 const Header = () => {
+  const pathname = usePathname();
+
   return (
     <Navbar className="w-[60%] h-16 mx-auto rounded-full border-1 border-sky-400 shadow-lg mt-5">
       <NavbarContent className="flex !justify-center w-full">
         <NavbarItem>
-          <Link color="foreground" href="/">
+          <Link className={pathname === "/" ? "text-sky-400" : ""} href="/">
             Home
           </Link>
         </NavbarItem>
         <NavbarItem>
-          <Link color="foreground" href="/about">
+          <Link
+            className={pathname === "/about" ? "text-sky-400" : ""}
+            href="/about"
+          >
             About
           </Link>
         </NavbarItem>
         <NavbarItem>
-          <Link color="foreground" href="/projects">
+          <Link
+            className={pathname === "/projects" ? "text-sky-400" : ""}
+            href="/projects"
+          >
             Projects
           </Link>
         </NavbarItem>
         <NavbarItem>
-          <Link color="foreground" href="/blogs">
+          <Link
+            className={pathname === "/blogs" ? "text-sky-400" : ""}
+            href="/blogs"
+          >
             Blogs
           </Link>
         </NavbarItem>
