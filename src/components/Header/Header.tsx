9c1e12ddaa4e05e51c8b0b49f249e3d417135660
mehrabhossain1 <mehrab.munna00@gmail.com>
2024-05-13 11:54:24 +0600

    header responsive

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -9,7 +9,7 @@ const Header = () => {
   const pathname = usePathname();
 
   return (
-    <Navbar className="w-[60%] h-16 mx-auto rounded-full border-1 border-sky-400 shadow-lg mt-5">
+    <Navbar className="w-full md:w-[60%] h-16 mx-auto rounded-full border-1 border-sky-400 shadow-lg mt-5">
       <NavbarContent className="flex !justify-center w-full">
         <NavbarItem>
           <Link className={pathname === "/" ? "text-sky-400" : ""} href="/">
