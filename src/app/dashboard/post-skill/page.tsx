aaf1266451a63ae6e895cb4e256dedc75f054608
mehrabhossain1 <mehrab.munna00@gmail.com>
2024-05-13 17:14:51 +0600

    skills: post, get

--- src/app/dashboard/post-skill/page.tsx
+++ src/app/dashboard/post-skill/page.tsx
@@ -1,7 +1,72 @@
+"use client";
+import { useRouter } from "next/navigation";
+import { useState } from "react";
+
 const PostSkillPage = () => {
+  const router = useRouter();
+  const [formData, setFormData] = useState({
+    title: "",
+    image: "",
+  });
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setFormData({ ...formData, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = async (e: React.FormEvent) => {
+    e.preventDefault();
+    try {
+      const response = await fetch(`http://localhost:5000/api/v1/skills`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(formData),
+      });
+      if (response.ok) {
+        console.log("Skill data posted successfully");
+        // Optionally, you can reset the form after successful submission
+        setFormData({
+          title: "",
+          image: "",
+        });
+
+        router.push("/skills");
+      } else {
+        console.error("Failed to post project data");
+      }
+    } catch (error) {
+      console.error("Error posting project data:", error);
+    }
+  };
+
   return (
     <div>
-      <h1>Post Skill Page</h1>
+      <form onSubmit={handleSubmit}>
+        <label htmlFor="title">Title:</label>
+        <br />
+        <input
+          type="text"
+          id="title"
+          name="title"
+          value={formData.title}
+          onChange={handleChange}
+        />
+        <br />
+
+        <label htmlFor="image">Image:</label>
+        <br />
+        <input
+          type="text"
+          id="image"
+          name="image"
+          value={formData.image}
+          onChange={handleChange}
+        />
+        <br />
+
+        <button type="submit">Post Project</button>
+      </form>
     </div>
   );
 };
