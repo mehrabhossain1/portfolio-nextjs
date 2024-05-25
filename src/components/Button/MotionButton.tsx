5fa25e89e0cda3456777ee4c3bcf6368a9cf8cff
mehrabhossain1 <mehrab.munna00@gmail.com>
2024-05-25 22:03:08 +0600

    comment

--- src/components/Button/MotionButton.tsx
+++ src/components/Button/MotionButton.tsx
@@ -8,6 +8,7 @@ const MotionButton = ({ children }: { children: React.ReactNode }) => {
       whileHover={{ scale: 1.05 }}
       whileTap={{ scale: 0.95 }}
       transition={{ duration: 0.2 }}
+      //
     >
       <Button className="bg-sky-400 font-semibold hover:bg-sky-500 focus:bg-sky-500 text-white">
         {children}
